6683b14d28d342ef25118d8f1d0eff1d0ac99347
bheang30 <solmorobhea20@gmail.com>
2024-09-10 11:16:53 +0800

    Create app.js

---
+++ variables.js/app.js
@@ -0,0 +1,9 @@
+let name = "Solmoro Bhea";
+let whatDoYouWannaBecomeinYourLife = "programmer";
+let gender = "female";
+let twitterHandele = "@SolmoroBhea";
+
+console.log('My name is ; name);
+console.log('I want to become a ; whatDoYouWannaBecomeinYourLife);
+console.log('My gender is ; gender);
+console.log('My twitter handle is ; twitterHandle);          
